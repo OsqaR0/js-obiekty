8e969fed6e2470537dbaa23851cf3d5ed07f4e09
OsqaR0 <106465473+OsqaR0@users.noreply.github.com>
2022-09-11 12:39:19 +0200

    Add files via upload

---
+++ moje.js
@@ -0,0 +1,15 @@
+let car1 = {
+    mark: 'BMW',
+    model: 'M3',
+    productionYear: '2012'
+};
+
+console.log(car1.mark+', '+car1.model+': '+car1.productionYear);
+
+let car2 = {
+    mark: 'VW',
+    model: 'Passat',
+    productionYear: '2006'
+};
+
+console.log(car2.mark+', '+car2.model+': '+car2.productionYear);
